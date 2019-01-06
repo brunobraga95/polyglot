ec7876a80fc20a156f14af2474721e74ca4c1017
brunobraga95 <brnbraga95@gmail.com>
2019-01-06 21:37:56 -0200

    add env vars

--- src/db/firestore/initialize.js
+++ src/db/firestore/initialize.js
@@ -2,7 +2,12 @@ const firebase = require("firebase");
 require("firebase/firestore");
 
 firebase.initializeApp({
-  ...
+  apiKey: process.env.REACT_APP_PAPI_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET_NAME,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 });
   
   // Initialize Cloud Firestore through Firebase
